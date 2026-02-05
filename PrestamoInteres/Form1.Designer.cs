84e809d41facffe01b32c5bf270e409b7b6f8920
Giovani4l <I24050379@monclova.tecnm.mx>
2026-02-04 20:08:39 -0600

    creacion de atributos y metodo para calcular la cuota

--- PrestamoInteres/Form1.Designer.cs
+++ PrestamoInteres/Form1.Designer.cs
@@ -28,18 +28,63 @@
         /// </summary>
         private void InitializeComponent()
         {
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            button1 = new Button();
             SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(312, 132);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(125, 27);
+            textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(312, 198);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(125, 27);
+            textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(312, 265);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(125, 27);
+            textBox3.TabIndex = 2;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(330, 355);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 3;
+            button1.Text = "Calcular";
+            button1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(button1);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
             Name = "Form1";
             Text = "Form1";
+            Load += Form1_Load;
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private Button button1;
     }
 }
--- PrestamoInteres/Form1.cs
+++ PrestamoInteres/Form1.cs
@@ -6,5 +6,26 @@ namespace PrestamoInteres
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        public class Prestamo
+        {
+            // Atributos / Propiedades
+            public double Monto { get; set; }
+            public double TasaInteresAnual { get; set; }
+            public int PlazoMeses { get; set; }
+
+            // Método para calcular la cuota mensual
+            public double CalcularCuota()
+            {
+                double tasaMensual = (TasaInteresAnual / 100) / 12;
+                double cuota = Monto * (tasaMensual * Math.Pow(1 + tasaMensual, PlazoMeses)) /
+                               (Math.Pow(1 + tasaMensual, PlazoMeses) - 1);
+                return Math.Round(cuota, 2);
+            }
+        }
     }
 }
